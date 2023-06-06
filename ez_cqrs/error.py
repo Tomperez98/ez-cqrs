c8109b6ae139ce1f7deebfedd2fc8f36aa024add
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-06-05 19:08:53 -0500

    Minimalistic approach

--- ez_cqrs/error.py
+++ ez_cqrs/error.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
         from typing_extensions import TypeAlias
 
 
-class UserError(Exception):
+class DomainError(Exception):
     """
     Raised when a user violates a business rule.
 
@@ -21,38 +21,6 @@ class UserError(Exception):
 
     This translates into a `Bad Request` status.
     """
-
-    def __init__(self, business_violation: Exception) -> None:  # noqa: D107
-        super().__init__(business_violation)
-
-
-class AggregateConflictError(Exception):
-    """
-    Raised when there's another command on the same aggregate instance.
-
-    This is handled by optimistic locking in systems backed by an RDBMS.
-
-    In a Restful app this translates into a `Too Many Requests` or `Service Unavailable`
-    response status, often with a [Retry-After response header](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After)
-    indicating that the user should try again.
-    """
-
-    def __init__(self) -> None:  # noqa: D107
-        super().__init__()
-
-
-class DatabaseConnectionError(Exception):
-    """Raised when an error ocurred while attempting to read/write from a database."""
-
-    def __init__(self) -> None:  # noqa: D107
-        super().__init__()
-
-
-class DeserializationError(Exception):
-    """Raised when a deserialization error ocurred due to invalid JSON."""
-
-    def __init__(self) -> None:  # noqa: D107
-        super().__init__()
 
 
 class UnexpectedError(Exception):
@@ -64,14 +32,11 @@ class UnexpectedError(Exception):
     investigation rather than returned to the user.
     """
 
-    def __init__(self) -> None:  # noqa: D107
-        super().__init__()
+    def __init__(self, unexpected_error: Exception) -> None:  # noqa: D107
+        super().__init__(f"Unexpected error {unexpected_error}")
 
 
-AggregateError: TypeAlias = Union[
-    UserError,
-    AggregateConflictError,
-    DatabaseConnectionError,
-    DeserializationError,
+ExecutionError: TypeAlias = Union[
+    DomainError,
     UnexpectedError,
 ]
--- ez_cqrs/testing/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from ez_cqrs.testing.framework import Framework
-
-__all__ = ["Framework"]
