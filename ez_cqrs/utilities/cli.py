296c692641268f1d1e230480c4a042afb43a1c4e
Tomas Perez Alvarez <72174660+Tomperez98@users.noreply.github.com>
2023-06-09 09:55:04 -0500

    Update cli.py

--- ez_cqrs/utilities/cli.py
+++ ez_cqrs/utilities/cli.py
@@ -3,35 +3,17 @@ from __future__ import annotations
 
 import asyncio
 from functools import wraps
-from typing import TYPE_CHECKING, Any, Protocol
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Coroutine
+    from collections.abc import Callable
 
 
-class Callback(Protocol):
-    """
-    Callback protocol for CLI commands.
-
-    See: https://mypy.readthedocs.io/en/latest/protocols.html#callback-protocols
-    """
-
-    def __call__(  # type:ignore[no-untyped-def]  # noqa: D102
-        self,
-        *args,  # noqa: ANN002
-        **kwargs,  # noqa: ANN003
-    ) -> Coroutine[Any, Any, None]:
-        ...
-
-
-def coro(f: Callback) -> Callable[[Any], Any]:
+def coro(f: Any) -> Callable[[Any], Any]:  # noqa: ANN401
     """Run CLI command as a coroutine."""
 
     @wraps(f)
-    def wrapper(  # type:ignore[no-untyped-def]
-        *args,  # noqa: ANN002
-        **kwargs,  # noqa: ANN003
-    ) -> Any:  # noqa: ANN401
+    def wrapper(*args, **kwargs) -> Any:  # type:ignore[no-untyped-def]  # noqa: ANN401
         return asyncio.run(f(*args, **kwargs))
 
     return wrapper
