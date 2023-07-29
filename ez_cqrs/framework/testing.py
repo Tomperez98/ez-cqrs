338c1014ed23007605f68c0c4ba48c880589d8d5
Tomperez98 <tomasperezalvarez@gmail.com>
2023-07-29 17:43:28 -0500

    Only check domain error

--- ez_cqrs/framework/testing.py
+++ ez_cqrs/framework/testing.py
@@ -3,14 +3,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Generic, final
 
+from result import Ok
+
 from ez_cqrs.components import C, E
+from ez_cqrs.error import DomainError
 
 if TYPE_CHECKING:
     from result import Result
 
     from ez_cqrs.acid_exec import ACID
     from ez_cqrs.components import UseCaseOutput
-    from ez_cqrs.error import DomainError
     from ez_cqrs.framework import EzCqrs
 
 
@@ -59,6 +61,11 @@ class EzCQRSTester(Generic[C, E]):
             app_database=self.app_database,
             event_registry=event_registry,
         )
+        if not isinstance(use_case_result, Ok):
+            error = use_case_result.err()
+            if not isinstance(error, DomainError):
+                msg = f"Encounter error is {error}"
+                raise RuntimeError(msg)
 
         return all(
             [use_case_result == expected_result, event_registry == expected_events],
