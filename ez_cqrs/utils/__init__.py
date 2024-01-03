3c5c35a19f23715685cc0ce211efc1ce47b11f47
Tomperez98 <tomasperezalvarez@gmail.com>
2024-01-03 16:12:35 -0500

    No more persistable

--- ez_cqrs/utils/__init__.py
+++ ez_cqrs/utils/__init__.py
@@ -1,6 +1,9 @@
 """Application utilities."""
 from __future__ import annotations
 
-from ez_cqrs.utils import cli, dates, models
+from ez_cqrs.utils import cli, dates
 
-__all__ = ["dates", "cli", "models"]
+__all__ = [
+    "dates",
+    "cli",
+]
