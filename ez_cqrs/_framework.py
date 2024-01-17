d73db1bec74e21d51592b650f51f64660ecda8c4
Tomperez98 <tomasperezalvarez@gmail.com>
2024-01-17 11:19:57 -0500

    Framework only domain error

--- ez_cqrs/_framework.py
+++ ez_cqrs/_framework.py
@@ -5,7 +5,7 @@ import asyncio
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Generic, final
 
-from result import Err, Ok
+from result import Ok
 
 from ez_cqrs._typing import T
 from ez_cqrs.components import (
@@ -37,7 +37,7 @@ class EzCqrs(Generic[R]):
         max_transactions: int,
         app_database: ACID[T] | None,
         events: list[E],
-    ) -> Result[R, DomainError | TransactionExecutionError]:
+    ) -> Result[R, DomainError]:
         """
         Validate and execute command, then dispatch command events.
 
@@ -54,13 +54,10 @@ class EzCqrs(Generic[R]):
             raise UnexpectedError(e) from e
 
         if app_database is not None:
-            commited_or_err = self._commit_existing_transactions(
+            self._commit_existing_transactions(
                 state_changes=state_changes,
                 app_database=app_database,
             )
-
-            if not isinstance(commited_or_err, Ok):
-                return commited_or_err
 
         asyncio.gather(*(event.publish() for event in events), return_exceptions=False)
 
@@ -73,16 +70,15 @@ class EzCqrs(Generic[R]):
         self,
         state_changes: StateChanges[T],
         app_database: ACID[T],
-    ) -> Result[None, TransactionExecutionError]:
+    ) -> None:
         if state_changes.storage_length() > 0:
             try:
                 app_database.commit_as_transaction(
                     ops_registry=state_changes,
                 )
             except Exception as e:  # noqa: BLE001
-                return Err(TransactionExecutionError(e))
+                raise TransactionExecutionError(e) from e
 
         if not state_changes.is_empty():
             msg = "Ops registry didn't came empty after transactions commit."
             raise RuntimeError(msg)
-        return Ok(None)
